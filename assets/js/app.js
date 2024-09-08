1a19a4ff1e3976959b4f04b7861bb0c3bf4218b3
W1zarD <96693781+Oleg19023@users.noreply.github.com>
2024-09-08 19:23:22 +0200

    Commit 3

---
+++ assets/js/app.js
@@ -0,0 +1,31 @@
+// Темная тема
+document.getElementById('themeToggle').addEventListener('click', function() {
+    let body = document.body;
+    let navbar = document.querySelector('.navbar');
+    let header = document.querySelector('header');
+    let navLinks = document.querySelectorAll('.nav-link');
+    let navbarBrandIcon = document.querySelector('.navbar-brand i');
+    let themeToggle = document.getElementById('themeToggle');
+    let cardBodies = document.querySelectorAll('.card-body');
+    let imgTheme = document.querySelectorAll('.img-theme');
+    let themeIcon = document.getElementById('themeIcon');
+    body.classList.toggle('dark-theme');
+    navbar.classList.toggle('dark-theme-navbar');
+    header.classList.toggle('dark-theme-header');
+    navLinks.forEach(navLink => navLink.classList.toggle('dark-theme-text'));
+    navbarBrandIcon.classList.toggle('dark-theme-icon');
+    themeToggle.classList.toggle('dark-theme-button');
+    imgTheme.forEach(img => img.classList.toggle('img-theme-dark'));
+    cardBodies.forEach(cardBody => cardBody.classList.toggle('card-body-theme'));
+    if (body.classList.contains('dark-theme')) {
+        themeIcon.classList.remove('fa-moon');
+        themeIcon.classList.add('fa-sun');
+    } else {
+        themeIcon.classList.remove('fa-sun');
+        themeIcon.classList.add('fa-moon');
+    }
+  });
+  // Цвет фона
+  document.getElementById('colorPicker').addEventListener('input', function() {
+    document.body.style.backgroundColor = this.value;
+  });
